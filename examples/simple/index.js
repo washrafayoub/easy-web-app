418517ee727b83826057a7f8671120cfcf26eaff
mh <m.e.harms@t-online.de>
2016-03-06 12:56:23 +0100

    static content

--- examples/simple/index.js
+++ examples/simple/index.js
@@ -2,5 +2,9 @@
 
 var gui = require('../../');
 
-gui.init();
+gui.staticCSS = __dirname + '../rest-web-ui/html/css/';
+gui.staticJS  = __dirname + '../rest-web-ui/html/js/';
+gui.staticImg = __dirname + '../rest-web-ui/html/img/';
+gui.staticMod = __dirname + '../rest-web-ui/html/modules/';
 
+gui.init();
--- index.js
+++ index.js
@@ -21,20 +21,12 @@ gui.init = function init( port ) {
 	
 	console.log( 'GUI: http://localhost:'+wsPort+'/' );
 	
-};
+	webservices.use( '/css', express.static( gui.staticCSS ) );
+	webservices.use( '/js', express.static( gui.staticJS ) );
+	webservices.use( '/img', express.static( gui.staticImg ) );
+	webservices.use( '/modules', express.static( gui.staticMod ) );
 
-/** REST web service to GET layout structure: */
-webservices.get(
-	'/svc/layout/:id', 
-	function( req, res ){
-		if (  gui.pages[ req.params.id ] ) {
-			res.send( JSON.stringify( gui.pages[req.params.id ] ) );
-		} else {
-		    res.statusCode = 404;
-		    return res.send('Error 404: No quote found');
-		}  
-	}
-);
+};
 
 /** Set defaults for all required configurations */
 gui.setDefaults = function setDefaults() {
@@ -49,6 +41,31 @@ gui.setDefaults = function setDefaults() {
 				'copyrightText':'powered by <a href="https://github.com/ma-ha/rest-web-ui">ReST-Web-GUI</a>' 
 			}
 	};
-
-
+	
+//	this.staticCSS =  __dirname + '/css';
+//	this.staticJS  =  __dirname + '/js';
+//	this.staticImg =  __dirname + '/img';
+//	this.staticMod =  __dirname + '/modules';
+	
 };
+
+
+/** REST web service to GET layout structure: */
+webservices.get(
+	'/svc/layout/:id', 
+	function( req, res ){
+		if (  gui.pages[ req.params.id ] ) {
+			res.send( JSON.stringify( gui.pages[req.params.id ] ) );
+		} else {
+		    res.statusCode = 404;
+		    return res.send('Error 404: No quote found');
+		}  
+	}
+);
+
+webservices.get( 
+	'/', 
+	function( req, res ) {
+		res.sendFile( __dirname + '/index.html' );
+	}
+);
