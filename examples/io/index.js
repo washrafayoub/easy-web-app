fc8958f1d2f878b1225d74fc35aa5c1330e14ffe
mh <m.e.harms@t-online.de>
2016-03-12 18:57:40 +0100

    improved code formatting of I/O example

--- examples/io/index.js
+++ examples/io/index.js
@@ -1,33 +1,31 @@
-var gui = require ( '../../' );
+const LED_GREEN = 1, LED_OFF = 0, SWITCH_ON = 'on', SWITCH_OFF = 'off'
+
+var gui = require( '../../' )
+var log = require( 'npmlog' )
 
 // Initialize the framework and the default page
-gui.init ();
-
-const LED_GREEN  = 1 
-    , LED_OFF    = 0
-    , SWITCH_ON  = "on"
-    , SWITCH_OFF = "off";
-
+gui.init()
+  
 // create a IO view on page
-var ioView = gui.addIoView ();
+var ioView = gui.addIoView()
 
 // change update speed to 1 sec (= 1000 ms)
-ioView.setUpdateMilliSec ( 10000 );
+ioView.setUpdateMilliSec( 10000 )
 
-// add a "LED" at x/y 100px/100px and stich it to "on" (=1)
-var led = LED_GREEN;
-ioView.addLED ( "myFirstLED", 200, 100, led );
+// add a 'LED' at x/y 100px/100px and stich it to 'on' (=1)
+var led = LED_GREEN
+ioView.addLED( 'myFirstLED', 200, 100, led )
 
-ioView.addSwitch ( 
-  "mySwitch",               // the ID
-  100, 100,                 // x,y pos of switch
-  [ SWITCH_ON, SWITCH_OFF ],  // possible values
-  function(switchValue) {
-    console.log ( "Switch now: " + switchValue );
-    if ( switchValue == SWITCH_ON ) {
-      ioView.setLED ( "myFirstLED", LED_GREEN );
-    } else {
-      ioView.setLED ( "myFirstLED", LED_OFF );
-    }
-  }
-);
+ioView.addSwitch( 
+    'mySwitch',                   // the ID
+    100, 100,                     // x,y pos of switch
+    [ SWITCH_ON, SWITCH_OFF ],    // possible values
+    function ( switchValue ) {
+      log.info( 'Switch "mySwitch"', 'now %j', switchValue );
+      if ( switchValue == SWITCH_ON ) {
+        ioView.setLED( 'myFirstLED', LED_GREEN );
+      } else {
+        ioView.setLED( 'myFirstLED', LED_OFF );
+      }
+    } 
+)
