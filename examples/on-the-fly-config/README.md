fa8fde3db7115cbda2c61a42a9490801c79422d4
mh <m.e.harms@t-online.de>
2016-03-26 19:56:01 +0100

    On-the-fly configuration exammple

---
+++ examples/on-the-fly-config/README.md
@@ -0,0 +1,8 @@
+## On-the-fly Config Demo
+This create a page with header, table and footer. 
+You can modify the table in the browser by clicking the configure icon (top right).
+
+## Run example
+1. You need to [get a local copy of the easy-web gui](https://github.com/ma-ha/easy-web-gui).
+2. Get required packages: `npm install`
+2. Then simply run `nodejs index.js` in this directoy 
---
+++ examples/on-the-fly-config/index.js
@@ -0,0 +1,114 @@
+/** Simple example: Create a web page with form */
+
+var gui = require ( '../../' )       // stand alone: relpace with  require( 'easy-web-app' )
+
+// initialize the web app framework and a default main page
+gui.init ()
+
+// add a view of type 'pong-easy-table' (= plug-in) 
+gui.addView ( 
+  // view config
+  {
+    id    : 'tableView',
+    title : 'Table Demo (sorry, zoom is always a flower ;-)',
+    type  : 'pong-easytable',
+    resourceURL : '/products',
+    actions:
+      [
+        {
+          type: 'pong-on-the-fly',
+          param: { assistUrl:"/products/pong-easytable/columns-available" }
+        }
+      ]
+  }
+  // plug-in config will be loadad from http://localhost:8888/products/pong-table 
+)
+
+// the table requires a REST web service to serve the data: 
+var svc  = gui.getExpress()
+var bodyParser  = require( 'body-parser' );
+var jsonParser  = bodyParser.json()
+
+// this is the table config at statup -- but you can change it in the browser
+var moduleConfig =  {
+    easyCols:
+      [
+        '*ID|5%',
+        'Name=Name.0',
+        'ProductPage_linkForCol_1',
+        'Picture',
+        'ZoomImg_zooms_Picture',
+        'Status_checkbox',
+        'Rating',
+        'Description_editable',
+        'Product~Page_link'
+      ],
+      maxRows: '5'
+  }
+var moduleConfigTemplate =  JSON.parse( JSON.stringify( moduleConfig ) ) // clone moduleConfig
+
+// load table config
+svc.get( 
+    '/products/pong-table', 
+    function( req, res ) {
+      res.json( moduleConfig )
+    }
+  )
+
+// serve available config columns
+svc.get( 
+    '/products/pong-easytable/columns-available', 
+    function( req, res ) {
+      res.send( JSON.stringify( moduleConfigTemplate, null, '\t' ) )
+    }
+  )
+
+// load table config
+svc.get( 
+    '/products/pong-easytable', 
+    function( req, res ) {
+      res.json( moduleConfig )
+    }
+  )
+  
+// save changed table config back
+svc.post( 
+    '/products/pong-easytable', 
+    jsonParser,
+    function( req, res ) {
+      //console.log( JSON.stringify( req.body ) )
+      if ( req.body && req.body.easyCols && req.body.maxRows ) {
+        moduleConfig = req.body
+        //console.log( 'Response 200' )
+        res.statusCode = 200  // request OK
+      } else {
+        //console.log( 'Response 200' )
+        res.statusCode = 400  // bad request
+      }
+      return res.json( moduleConfig ) 
+    }
+  )
+
+svc.get( 
+    '/products', 
+    function( req, res ) {
+      // generate some dummy data:
+      var tableData = 
+        [ 
+         {"ID":"yyy1","Name":["Prod A","A"],"Rating":"3","Status":"false","ProductPage":"http:\/\/mh-svr.de\/pong_dev\/README.md","descr":"Blah blub bubber.","Picture":"img\/x02.png","ZoomImg":"img\/tst.jpg"},
+         {"ID":"yyy3","Name":["Prod B","A"],"Rating":"2","Status":"true","ProductPage":"http:\/\/mh-svr.de\/pong_dev\/README.md","descr":"Blah blub bubber.","Picture":"img\/x03.png","ZoomImg":"img\/tst.jpg"},
+         {"ID":"yyy4","Name":["Prod C","A"],"Rating":"2","Status":"true","ProductPage":"http:\/\/mh-svr.de\/pong_dev\/README.md","descr":"Blah blub bubber.","Picture":"img\/x04.png","ZoomImg":"img\/tst.jpg"},
+         {"ID":"yyy5","Name":["Prod D","A"],"Rating":"2","Status":"true","ProductPage":"http:\/\/mh-svr.de\/pong_dev\/README.md","descr":"Blah blub bubber.","Picture":"img\/x05.png","ZoomImg":"img\/tst.jpg"},
+         {"ID":"yyy6","Name":["Prod E","A"],"Rating":"1","Status":"false","ProductPage":"http:\/\/mh-svr.de\/pong_dev\/README.md","descr":"Blah blub bubber.","Picture":"img\/x06.png","ZoomImg":"img\/tst.jpg"},
+         {"ID":"yyy7","Name":["Prod F","A"],"Rating":"2","Status":"false","ProductPage":"http:\/\/mh-svr.de\/pong_dev\/README.md","descr":"Blah blub bubber.","Picture":"img\/x07.png","ZoomImg":"img\/tst.jpg"},
+         {"ID":"yyy8","Name":["Prod G","A"],"Rating":"2","Status":"true","ProductPage":"http:\/\/mh-svr.de\/pong_dev\/README.md","descr":"Blah blub bubber.","Picture":"img\/x08.png","ZoomImg":"img\/tst.jpg"},
+         {"ID":"yy09","Name":["Prod I","A"],"Rating":"3","Status":"true","ProductPage":"http:\/\/mh-svr.de\/pong_dev\/README.md","descr":"Blah blub bubber.","Picture":"img\/x10.png","ZoomImg":"img\/tst.jpg"},
+         {"ID":"yy19","Name":["Prod J","A"],"Rating":"3","Status":"true","ProductPage":"http:\/\/mh-svr.de\/pong_dev\/README.md","descr":"Blah blub bubber.","Picture":"img\/x11.png","ZoomImg":"img\/tst.jpg"},
+         {"ID":"yy11","Name":["Prod K","A"],"Rating":"2","Status":"true","ProductPage":"http:\/\/mh-svr.de\/pong_dev\/README.md","descr":"Blah blub bubber.","Picture":"img\/x12.png","ZoomImg":"img\/tst.jpg"},
+         {"ID":"yy12","Name":["Prod L","A"],"Rating":"2","Status":"true","ProductPage":"http:\/\/mh-svr.de\/pong_dev\/README.md","descr":"Blah blub bubber.","Picture":"img\/x13.png","ZoomImg":"img\/tst.jpg"},
+         {"ID":"yy13","Name":["Prod M","A"],"Rating":"2","Status":"true","ProductPage":"http:\/\/mh-svr.de\/pong_dev\/README.md","descr":"Blah blub bubber.","Picture":"img\/x14.png","ZoomImg":"img\/tst.jpg"},
+         {"ID":"yy14","Name":["Prod N","A"],"Rating":"2","Status":"true","ProductPage":"http:\/\/mh-svr.de\/pong_dev\/README.md","descr":"Blah blub bubber.","Picture":"img\/x15.png","ZoomImg":"img\/tst.jpg"}
+        ]
+      res.json( tableData )
+    }
+  )
