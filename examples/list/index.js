99b0364ae0640e23c6b4afafe8c649d9fd0db94b
mh <m.e.harms@t-online.de>
2016-10-07 17:45:25 +0200

    List example with pie charts

--- examples/list/index.js
+++ examples/list/index.js
@@ -33,6 +33,15 @@ mainPage.addView(
               { id:'CustomerCount', cellType:'text' }
             ]
           },  
+          { "id" : "XSizeG", "cellType" : "div", 
+            "divs" : [ 
+                { "id" : "SizeGlb", "cellType" : "label", "label" : "Sizing" }, 
+                { "id" : "SizeStat", "cellType": "pie",
+                  "min":"0", "max":"100",
+                  "labelColor":"#FFF"
+                }
+              ]
+          },
           { id:'XSize', cellType:'div',
             'divs': [
                { id:'Sizelb', cellType:'label', label:'Sizing:' },
@@ -84,10 +93,31 @@ svc.get(
     // generate some dummy data:
     var tableData = 
       [ 
-       {"ID":"ABC","CustomerCount":"1234","Rating":"3","Status":"false","Size":{"S":"40%","Strend":"arrow-1-e","M":"30%","Mtrend":"arrow-1-e","L":"30%","Ltrend":"arrow-1-ne"},"Orders":{"D":"4","W":"11","M":"123","Dtrend":"arrow-1-n","Wtrend":"arrow-1-e","Mtrend":"arrow-1-se"},"Availability":"<span style=\"color:green\">100%</span>"},
-       {"ID":"XYZ","CustomerCount":"345","Rating":"2","Status":"true","Size":{"S":"40%","Strend":"arrow-1-e","M":"30%","Mtrend":"arrow-1-se","L":"30%","Ltrend":"arrow-1-ne"},"Orders":{"D":"4","W":"11","M":"123","Dtrend":"arrow-1-ne","Wtrend":"arrow-1-e","Mtrend":"arrow-1-se"},"Availability":"<span style=\"color:orange\">99,96%</span>"},
-       {"ID":"A123","CustomerCount":"678","Rating":"2","Status":"true","Size":{"S":"40%","Strend":"arrow-1-se","M":"30%","Mtrend":"arrow-1-n","L":"30%","Ltrend":"arrow-1-e"},"Orders":{"D":"4","W":"11","M":"123","Dtrend":"arrow-1-s","Wtrend":"arrow-1-e","Mtrend":"arrow-1-se"},"Availability":"<span style=\"color:green\">100%</span>"},
-       {"ID":"YIII","CustomerCount":"900","Rating":"2","Status":"true","Size":{"S":"40%","Strend":"arrow-1-e","M":"30%","Mtrend":"arrow-1-n","L":"30%","Ltrend":"arrow-1-se"},"Orders":{"D":"4","W":"11","M":"123","Dtrend":"arrow-1-ne","Wtrend":"arrow-1-e","Mtrend":"arrow-1-se"},"Availability":"<span style=\"color:red\">88,5%</span>"}
+       {"ID":"ABC","CustomerCount":"1234","Rating":"3","Status":"false",
+         "SizeStat":[
+                     {"val":"40","label":"S","color":"#0aa"},
+                     {"val":"30","label":"M","color":"#0cc"},
+                     {"val":"30","label":"L","color":"#0ee"}],
+         "Size":{"S":"40%","Strend":"arrow-1-e","M":"30%","Mtrend":"arrow-1-e","L":"30%","Ltrend":"arrow-1-ne"},
+         "Orders":{"D":"4","W":"11","M":"123","Dtrend":"arrow-1-n","Wtrend":"arrow-1-e","Mtrend":"arrow-1-se"},"Availability":"<span style=\"color:green\">100%</span>"},
+       {"ID":"XYZ","CustomerCount":"345","Rating":"2","Status":"true",
+           "SizeStat":[{"val":"10","label":"S","color":"#0aa"},
+                       {"val":"10","label":"M","color":"#0cc"},
+                       {"val":"80","label":"L","color":"#0ee"}],
+           "Size":{"S":"10%","Strend":"arrow-1-e","M":"10%","Mtrend":"arrow-1-se","L":"80%","Ltrend":"arrow-1-ne"},
+           "Orders":{"D":"4","W":"11","M":"123","Dtrend":"arrow-1-ne","Wtrend":"arrow-1-e","Mtrend":"arrow-1-se"},"Availability":"<span style=\"color:orange\">99,96%</span>"},
+       {"ID":"A123","CustomerCount":"678","Rating":"2","Status":"true",
+         "SizeStat":[{"val":"20","label":"S","color":"#0aa"},
+                     {"val":"80","label":"M","color":"#0cc"},
+                     {"val":"20","label":"L","color":"#0ee"}],
+         "Size":{"S":"20%","Strend":"arrow-1-se","M":"80%","Mtrend":"arrow-1-n","L":"20%","Ltrend":"arrow-1-e"},
+         "Orders":{"D":"4","W":"11","M":"123","Dtrend":"arrow-1-s","Wtrend":"arrow-1-e","Mtrend":"arrow-1-se"},"Availability":"<span style=\"color:green\">100%</span>"},
+       {"ID":"YIII","CustomerCount":"900","Rating":"2","Status":"true",
+         "SizeStat":[{"val":"70","label":"S","color":"#0aa"},
+                     {"val":"30","label":"M","color":"#0cc"},
+                     {"val":"0","label":"L","color":"#0ee"}],
+         "Size":{"S":"70%","Strend":"arrow-1-e","M":"30%","Mtrend":"arrow-1-n","L":"0%","Ltrend":"arrow-1-se"},
+         "Orders":{"D":"4","W":"11","M":"123","Dtrend":"arrow-1-ne","Wtrend":"arrow-1-e","Mtrend":"arrow-1-se"},"Availability":"<span style=\"color:red\">88,5%</span>"}
       ]
     res.json( tableData )
   }
