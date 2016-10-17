dddb5783dff0a57ab3451692e56ae5a9255451de
mh <m.e.harms@t-online.de>
2016-10-17 09:48:29 +0200

    comments

--- examples/icons/index.js
+++ examples/icons/index.js
@@ -1,6 +1,6 @@
 var gui = require( '../../' )     // stand alone: replace with  require( 'easy-web-app' ) 
 
-// Initialize the framework and the default page
+// the icon navigation must appear on each page:
 
 var icons = { id:'nav1', title:' ', width:'7% ', type:'pong-icons' }
 var iconNav =
@@ -22,7 +22,7 @@ mainPage.header.modules = [] // removes nav-bar module
 var columns = mainPage.addColumnsRow( 'row3', '600px' )
 var nav = columns.addView( icons, iconNav )
 nav.decor = null
-// do business logic here:
+// empty view: do business logic here:
 columns.addView( { 'id':'col1view', 'title':'Main Page', 'width':'93%' } )
   
 
@@ -31,10 +31,10 @@ columns.addView( { 'id':'col1view', 'title':'Main Page', 'width':'93%' } )
 
 var page2 = gui.addPage( 'page2', 'Page 2' )
 var p2columns = page2.addColumnsRow( 'row3', '600px' )
-var nav = p2columns.addView( icons, iconNav )
-nav.decor = ''
+var nav2 = p2columns.addView( icons, iconNav )
+nav2.decor = ''
 
-// do business logic here:
+// empty view: do business logic here:
 p2columns.addView( { 'id':'col1view', 'title':'Page 2', 'width':'93%' } )
 
 
@@ -43,8 +43,8 @@ p2columns.addView( { 'id':'col1view', 'title':'Page 2', 'width':'93%' } )
 
 var page3 = gui.addPage( 'page3', 'Page 3' )
 var p3columns = page3.addColumnsRow( 'row3', '600px' )
-var nav = p3columns.addView( icons, iconNav )
-nav.decor = ''
+var nav3 = p3columns.addView( icons, iconNav )
+nav3.decor = ''
 
-// do business logic here:
+// empty view: do business logic here:
 p3columns.addView( { 'id':'col1view', 'title':'Page 3', 'width':'93%' } )
