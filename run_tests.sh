a0fe5b76206b86508e7651ba0f936ba988d8c271
washrafayoub <77836815+washrafayoub@users.noreply.github.com>
2021-01-30 17:04:16 +0200

    Update run_tests.sh
    actualTests

--- run_tests.sh
+++ run_tests.sh
@@ -3,4 +3,24 @@
 for i in `ls mocha-tests/test/`; do
   echo $i
 done
+
+
+### Start running testcases ###
+cd mocha-tests
+npm install -g mocha
+npm install zombie
+
+## The config test must be done separately:
+cd config-test
+mocha -t 10000 config.js
+cd ../
+
+### Run the tests
+for i in test/*; do
+  echo "TestName: $i"
+  mocha -t 10000 $i
+  echo "Exit Status: $?"
+done
+
+############
 echo 'FINISHED TESTING'
