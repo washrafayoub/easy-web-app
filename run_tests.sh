bb08654aabc48a36f4c8e943c1e91c9d1beccc76
washrafayoub <77836815+washrafayoub@users.noreply.github.com>
2021-01-30 17:25:10 +0200

    Update run_tests.sh
    onlyOne

--- run_tests.sh
+++ run_tests.sh
@@ -16,11 +16,11 @@ mocha -t 10000 config.js
 cd ../
 
 ### Run the tests
-for i in test/*; do
-  echo "TestName: $i"
-  mocha -t 10000 $i
-  echo "Exit Status: $?"
-done
-
+#for i in test/*; do
+#  echo "TestName: $i"
+#  mocha -t 10000 $i
+#  echo "Exit Status: $?"
+#done
+mocha -t 10000 test/wiki-demo.js
 ############
-echo 'FINISHED TESTING'
+#echo 'FINISHED TESTING'
